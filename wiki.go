f4f56152ebf0a74573ec7b63a47accf1f7f5c6b4
Yusuke Aono <yusukeaono.1031@gmail.com>
2023-06-09 13:48:31 +0900

    Add handler

--- wiki.go
+++ wiki.go
@@ -2,22 +2,15 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"log"
+	"net/http"
 )
 
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := os.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func main() {
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	p1.save()
-
-	p2, _ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
